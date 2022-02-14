03cd3cd0b8e6cd6e59744e55ffdb72d4f58a1bdd
chitra <chitraksonawane@gmail.com>
2022-02-14 14:10:54 -0600

    Added chassis drive motors hardware mapping

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/FFRobot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/FFRobot.java
@@ -1,13 +1,32 @@
 package org.firstinspires.ftc.teamcode.robot;
 
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.teamcode.Drive.MecanumDrive;
 
-public class FFRobot {
-    public MecanumDrive drive = new MecanumDrive();
-    public void init(HardwareMap hwmap)
-    {
-        drive.init(hwmap);
+public  class FFRobot {
+    HardwareMap hwMap = null;
+
+    //creating and initializing four dc motor objects for all the drive motors on robot
+    public DcMotor frontLeftMotor = null;
+    public DcMotor backLeftMotor = null;
+    public DcMotor frontRightMotor = null;
+    public DcMotor backRightMotor = null;
+
+    public void init(HardwareMap ahwMap) {
+        // save reference to hardware map
+        hwMap = ahwMap;
+
+        //hardware mapping dc motors to configuration files
+        frontLeftMotor = hwMap.dcMotor.get("FrontLeft");
+        backLeftMotor = hwMap.dcMotor.get("BackLeft");
+        frontRightMotor = hwMap.dcMotor.get("FrontRight");
+        backRightMotor = hwMap.dcMotor.get("BackRight");
+
+        //setting both the right motors in reverse direction
+        frontRightMotor.setDirection(DcMotor.Direction.REVERSE);
+        backRightMotor.setDirection(DcMotor.Direction.REVERSE);
     }
 }
