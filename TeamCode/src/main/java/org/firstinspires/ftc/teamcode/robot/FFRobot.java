dec7e92a306c720f2bbfa3074b1006f99ebb4fd0
chitra <chitraksonawane@gmail.com>
2022-02-14 13:05:57 -0600

    First commit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/FFRobot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/FFRobot.java
@@ -2,6 +2,12 @@ package org.firstinspires.ftc.teamcode.robot;
 
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
+import org.firstinspires.ftc.teamcode.Drive.MecanumDrive;
+
 public class FFRobot {
-    HardwareMap hwMap = null;
+    public MecanumDrive drive = new MecanumDrive();
+    public void init(HardwareMap hwmap)
+    {
+        drive.init(hwmap);
+    }
 }
